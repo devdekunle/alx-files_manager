e371fd9c16464755de86a67f11bf52557eaf8adb
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-09-11 04:54:53 -0700

    fix response statuscode

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,16 +4,14 @@ import dbClient from '../utils/db';
 class AppController {
   static getStatus(req, res) {
     if (redisClient.isAlive() && dbClient.isAlive()) {
-      res.statusCode = 200;
-      res.json({ redis: true, db: true });
+      res.status(200).json({ redis: true, db: true });
     }
   }
 
   static async getStats(req, res) {
-    res.statusCode = 200;
     const userCount = await dbClient.nbUsers();
     const fileCount = await dbClient.nbFiles();
-    res.json({ users: userCount, files: fileCount });
+    res.status(200).json({ users: userCount, files: fileCount });
   }
 }
 module.exports = AppController;
