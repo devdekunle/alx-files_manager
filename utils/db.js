bef2c364e6465a01c82e57f81ed4695e3bd4f495
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-09-18 22:21:23 +0100

    set up mongo connection string

--- utils/db.js
+++ utils/db.js
@@ -2,8 +2,7 @@ const { MongoClient } = require('mongodb');
 
 class DBClient {
   constructor() {
-    const url = 'mongodb+srv://devdekunle:adekunle9102@letshare.ps7luan.mongodb.net/?retryWrites=true&w=majority';
-    this.client = new MongoClient(url);
+    this.client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
     this.client.connect();
     this.db = this.client.db('files_manager');
 
