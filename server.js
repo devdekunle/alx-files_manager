c98e5c7939c12e6a03e28f1e8329df3f6a7da27f
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-09-29 00:02:04 +0100

    test this

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const routes = require('./routes/index')
 
 
 const app = express()
-const host = '0.0.0.0'
-const port = process.env.PORT | | 5000
+const host = 'localhost'
+const port = process.env.PORT || 5000
 app.use('/', routes)
 
 app.listen(port, host, ()=> {
